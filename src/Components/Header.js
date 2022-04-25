0e8d85f60a11c47c5069ae435c8cb197c8157035
Atul-837 <atul23547@gmail.com>
2022-04-25 16:51:07 -0400

    Uncontrolled forms with Modal

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,66 +1,158 @@
-import React, {Component} from "react";
-import Jumbotron from "react-bootstrap"
-import { Navbar, NavbarBrand,NavbarToggler, Nav, Collapse, NavItem, NavbarText } from "reactstrap";
-import {NavLink} from "react-router-dom"
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  Collapse,
+  NavItem,
+  NavbarText,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  FormGroup,
+  Input,
+  Label,
+  Form,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
-class Header extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isNavOpen: false
-            }
-            this.toggleNav = this.toggleNav.bind(this)
-    }
-    toggleNav(){
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        })
-    }
-    render(){
-        return (
-          <>
-            <Navbar dark color="dark" expand="md">
-              <div className="container">
-                <NavbarToggler onClick={this.toggleNav} />
-                <Collapse isOpen={this.state.isNavOpen} navbar className="justify-content-center">
-                  <Nav navbar >
-                    <NavItem>
-                      <NavLink className="nav-link" to="/home">
-                        <span className="fa fa-home fa-lg"></span>HOME
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/aboutus">
-                        <span className="fa fa-info fa-lg"></span>ABOUT US
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/menu">
-                        <span className="fa fa-list fa-lg"></span>MENU
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/contactus">
-                        <span className="fa fa-address-card fa-lg"></span>
-                        CONTACT US
-                      </NavLink>
-                    </NavItem>
-                  </Nav>
-                </Collapse>
-              </div>
-            </Navbar>
-            <div className="jumbotron jumbotron-fluid" style={{backgroundColor: "black"}}>
-              <div className="container d-flex align-items-center">
-                <h1 className="display-4">
-                  <img src="./images/HR.png" />
-                </h1>
-                <p className="lead" style={{color: "aliceblue"}}>ROAD TO SPECIALITY FOOD MADE IN HIMALAYA'S SERVED IN PLAINS </p>
-              </div>
-            </div>
-          </>
-        );
-    }
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleLogin() {
+    this.toggleModal();
+    alert(
+      "User: " +
+        this.username.value +
+        "Password: " +
+        this.password.value +
+        "Remember: " +
+        this.remember.checked
+    );
+   
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar dark color="dark" expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse
+              isOpen={this.state.isNavOpen}
+              navbar
+              className="justify-content-center"
+            >
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"></span>HOME
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <span className="fa fa-info fa-lg"></span>ABOUT US
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/menu">
+                    <span className="fa fa-list fa-lg"></span>MENU
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <span className="fa fa-address-card fa-lg"></span>
+                    CONTACT US
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg" />
+                    LOG IN
+                  </Button>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+        <div
+          className="jumbotron jumbotron-fluid"
+          style={{ backgroundColor: "black" }}
+        >
+          <div className="container d-flex align-items-center">
+            <h1 className="display-4">
+              <img src="./images/HR.png" />
+            </h1>
+            <p className="lead" style={{ color: "aliceblue" }}>
+              ROAD TO SPECIALITY FOOD MADE IN HIMALAYA'S SERVED IN PLAINS{" "}
+            </p>
+          </div>
+        </div>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlfor="username">UserName</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlfor="password">PassWord</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember Me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="dark">
+                LOG IN
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
 }
 
-
-export default Header
+export default Header;
